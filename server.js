3e373204549a168336f62451a978c2fc8eca5cf1
Xander <xander.cage.botha@gmail.com>
2025-09-28 15:55:26 +0100

    Fix CORS for Vercel frontend

--- server.js
+++ server.js
@@ -9,7 +9,8 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'https://reddit-clone-rust-eta.vercel.app'
+    'https://reddit-clone-rust-eta.vercel.app',
+    'https://reddit-clone-ioe-vercel.app'
   ]
 }));
 
